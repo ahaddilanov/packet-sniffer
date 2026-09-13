2cc3a3c997c0df56e9f5313656c23e6a1ea35ea5
Ahad <ahaddilanov@gmail.com>
2026-09-13 13:55:14 +0400

    Add packet header parsing for source, destination, and protocol

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,6 +1,11 @@
-from scapy.all import sniff
+from scapy.all import sniff, IP
 
 def process_packet(packet):
-    print(packet.summary())
+    if packet.haslayer(IP):
+        src_ip = packet[IP].src
+        dst_ip = packet[IP].dst
+        protocol = packet[IP].proto
+
+        print(f"Source: {src_ip} -> Destination: {dst_ip} | Protocol: {protocol}")
 
 sniff(prn=process_packet, count=10)
